267bdc18325bef46694cd92d305bc30aad41f5e7
Vlad Korzun <vlad.korzun.minsk@gmail.com>
2024-04-21 13:07:52 +0300

    create and realise class with functions

--- simplestack.py
+++ simplestack.py
@@ -1,2 +1,23 @@
 from collections import deque
 
+class Stack():
+    """Simple stack to work with deque through 5th lab"""
+    def __init__(self, data = []):
+        self.items = deque(data)
+
+    def __len__(self):
+        return len(self.items)
+    
+    def is_empty(self):
+        return bool(len(self))
+
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        if len(self) <= 0: print("stack is empty")
+        return self.items.pop()
+    
+    def peek(self):
+        if len(self) <= 0: print("stack is empty")
+        else: return self.items[-1]
