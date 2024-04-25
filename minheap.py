28c449da458b0f57fe0b99e0da7d825004c69ddf
Vlad Korzun <vlad.korzun.minsk@gmail.com>
2024-04-25 20:55:18 +0300

    finish class minheap

--- minheap.py
+++ minheap.py
@@ -19,33 +19,40 @@ class MinHeap:
 
     def pop(self):
         self.items[0], self.items[-1] = self.items[-1], self.items[0]
-        self.items.pop()
+        popped = self.items.pop()
         self.sift_down(0)
+        return popped
 
     def get_data(self):
         return self.items
     
     def sift_up(self, i):
-        ...
-
-    
-    def sift_down(self, i):
-        while self.items[i] < self.items[(i-1)//2]:
+        while i >= 1 and self.items[i] < self.items[(i-1)//2]:
             self.items[i], self.items[(i-1)//2] = self.items[(i-1)//2], self.items[i]
             i = (i-1)//2
 
+    def sift_down(self, i):
+        while (2*i + 1 < len(self) and self.items[i] > self.items[2*i + 1]) or (2*i + 2 < len(self) and self.items[i] > self.items[2*i + 2]):
+            if (2*i + 2 < len(self) and self.items[2*i + 2] > self.items[2*i + 1]):
+                self.items[i], self.items[2*i + 1] = self.items[2*i + 1], self.items[i]
+                i = 2*i + 1
+            elif (2*i + 2 < len(self)):
+                self.items[i], self.items[2*i + 2] = self.items[2*i + 2], self.items[i]
+                i = 2*i + 2
+            else:
+                self.items[i], self.items[2*i + 1] = self.items[2*i + 1], self.items[i]
+                i = 2*i + 1
 
 
-# from time import time
-# from random import random
+
+# from random import randrange
 # if __name__ == '__main__':
 
-#     start = time()
-#     s1 = Stack()
-#     for _ in range(100000):
-#         s1.enqueue(random())
+#     h1 = MinHeap()
+#     for _ in range(10):
+#         h1.push(randrange(1, 100))
     
-#     while not s1.is_empty():
-#         s1.dequeue()
+#     while not h1.is_empty():
+#         print(h1.pop(), end = ' ')
     
-#     print(time() - start, s1.peek())
+#     print(h1.peek())
